5c4d3a3ed783332a97ee68f3ea68d0ae48953116
TerrenceTang0705 <61458235+TerrenceTang0705@users.noreply.github.com>
2021-08-27 23:33:55 +0100

    junit change

--- lib/src/test/java/SkyInterviewTest/F1DriverJobTest.java
+++ lib/src/test/java/SkyInterviewTest/F1DriverJobTest.java
@@ -1,10 +1,11 @@
 package SkyInterviewTest;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import SkyInterview.F1DriverJob;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,7 +18,7 @@ public class F1DriverJobTest {
     	System.out.println("Test 1(no input file)");
     	//no input file
     	F1DriverJob classUnderTest = new F1DriverJob("noInputFile.csv", "resultFile1.csv");
-        assertTrue("executeJob() should return -1", classUnderTest.executeJob()==-1);
+        assertTrue(classUnderTest.executeJob()==-1, "executeJob() should return -1");
     }
     
     @Test 
@@ -25,7 +26,7 @@ public class F1DriverJobTest {
     	System.out.println("Test 2 (output path not exists)");
     	//output path not exists
     	F1DriverJob classUnderTest = new F1DriverJob("input.csv", "C:\\abc\\aaa\\resultFile1.csv");
-    	assertTrue("executeJob() should return -1", classUnderTest.executeJob()==-1);
+    	assertTrue(classUnderTest.executeJob()==-1, "executeJob() should return -1");
     }
     
     @Test 
@@ -33,7 +34,7 @@ public class F1DriverJobTest {
     	System.out.println("Test 3 (All 1)");
     	F1DriverJob classUnderTest = new F1DriverJob("inputAllSameOne.csv", "inputAllSameOne_Result.csv");
     	
-    	assertTrue("executeJob() should return 0", classUnderTest.executeJob()==0);
+    	assertTrue(classUnderTest.executeJob()==0, "executeJob() should return 0");
     	try {
 			List<String> listStr = Files.readAllLines(Path.of("inputAllSameOne_Result.csv"));
 			for (String currStr:listStr) {
@@ -56,7 +57,7 @@ public class F1DriverJobTest {
     	System.out.println("Test 4 (Empty input file)");
     	//output path not exists
     	F1DriverJob classUnderTest = new F1DriverJob("inputEmpty.csv", "inputEmpty_Result.csv");
-    	assertTrue("executeJob() should return -1 if input file is empty", classUnderTest.executeJob()==-1);
+    	assertTrue(classUnderTest.executeJob()==-1, "executeJob() should return -1 if input file is empty");
     }
     
     @Test 
@@ -64,10 +65,10 @@ public class F1DriverJobTest {
     	System.out.println("Test 5 (Less then 3 drivers)");
     	//output path not exists
     	F1DriverJob classUnderTest = new F1DriverJob("inputLessThanThreeDrivers.csv", "inputLessThanThreeDrivers_Result.csv");
-    	assertTrue("executeJob() should return 0", classUnderTest.executeJob()==0);
+    	assertTrue(classUnderTest.executeJob()==0, "executeJob() should return 0");
     	try {
 			List<String> listStr = Files.readAllLines(Path.of("inputLessThanThreeDrivers_Result.csv"));
-			assertTrue("result file should be non-empty with less than 3 lines", listStr.size()<3);
+			assertTrue(listStr.size()<3, "result file should be non-empty with less than 3 lines");
 		} catch (IOException e) {
 			//should not happen as executeJob return 0 which means ended successfully
 			fail("no output file.");
@@ -81,7 +82,7 @@ public class F1DriverJobTest {
     public void testF1DriverJobExexcutionInvalidFormat_NoComma () {
     	System.out.println("Test 6 (No Comma)");
     	F1DriverJob classUnderTest = new F1DriverJob("inputNoComma.csv", "inputNoComma_Result.csv");
-    	assertTrue("executeJob() should return -1", classUnderTest.executeJob()==-1);
+    	assertTrue(classUnderTest.executeJob()==-1, "executeJob() should return -1");
     	
     }
     
@@ -89,28 +90,28 @@ public class F1DriverJobTest {
     public void testF1DriverJobExexcutionInvalidFormat_MoreThanOneComma () {
     	System.out.println("Test 7 (More than 1 Comma)");
     	F1DriverJob classUnderTest = new F1DriverJob("inputMoreThanOneComma.csv", "inputMoreThanOneComma_Result.csv");
-    	assertTrue("executeJob() should return -1", classUnderTest.executeJob()==-1);
+    	assertTrue(classUnderTest.executeJob()==-1, "executeJob() should return -1");
     }
     
     @Test 
     public void testF1DriverJobExexcutionInvalidFormat_NoTime() {
     	System.out.println("Test 8 (no time)");
     	F1DriverJob classUnderTest = new F1DriverJob("inputNoTime.csv", "inputNoTime_Result.csv");
-    	assertTrue("executeJob() should return -1", classUnderTest.executeJob()==-1);
+    	assertTrue(classUnderTest.executeJob()==-1, "executeJob() should return -1");
     }
     
     @Test 
     public void testF1DriverJobExexcutionInvalidFormat_TimeInvalidFormat() {
     	System.out.println("Test 9 (time invalid)");
     	F1DriverJob classUnderTest = new F1DriverJob("inputTimeInvalid.csv", "inputTimeInvalid_Result.csv");
-    	assertTrue("executeJob() should return -1", classUnderTest.executeJob()==-1);
+    	assertTrue(classUnderTest.executeJob()==-1, "executeJob() should return -1");
     }
     
     @Test
     public void testF1DriverJobExexcutionInvalidFormat_NoDriverName() {
     	System.out.println("Test 10 (No driver name)");
     	F1DriverJob classUnderTest = new F1DriverJob("inputNoDriverName.csv", "inputNoDriverName_Result.csv");
-    	assertTrue("executeJob() should return -1", classUnderTest.executeJob()==-1);
+    	assertTrue(classUnderTest.executeJob()==-1, "executeJob() should return -1");
     }
     
 }
